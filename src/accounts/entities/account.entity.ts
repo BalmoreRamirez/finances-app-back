5f4e329896aa4acc75163b0ce9c4f4f002fe226e
Balmore Ramirez <jbramirez@innovacion.gob.sv>
2025-07-11 15:55:16 -0600

    fix: correct return types in JSDoc comments and improve code consistency

---
+++ src/accounts/entities/account.entity.ts
@@ -0,0 +1,63 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { AccountType } from '../../account-types/entities/account-type.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity('accounts')
+export class Account {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'int',
+    nullable: false,
+    comment: 'ID del usuario propietario de la cuenta',
+  })
+  user_id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+    nullable: false,
+    comment: 'Nombre de la cuenta',
+  })
+  account_name: string;
+
+  @Column({
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+    default: 0.0,
+    nullable: false,
+    comment: 'Saldo de la cuenta',
+  })
+  balance: number;
+
+  @ManyToOne(() => AccountType)
+  @JoinColumn({ name: 'account_type_id' })
+  account_type: AccountType;
+
+  @Column({
+    type: 'int',
+    nullable: false,
+    comment: 'ID del tipo de cuenta',
+  })
+  account_type_id: number;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    comment: 'Fecha de creación',
+  })
+  createdAt: Date;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+}
